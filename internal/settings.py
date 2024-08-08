d6fefa1a154b0811e3d6721f39961a9a085936fa
João Barreto 255 <34463115+JoaoBarreto255@users.noreply.github.com>
2024-08-07 22:13:52 -0300

    improved: rename, validate settings entries

--- internal/settings.py
+++ internal/settings.py
@@ -1,17 +1,19 @@
 """Aplication settings"""
 
+from pydantic import AmqpDsn, AnyUrl, Field, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ApiSettings(BaseSettings):
     app_env: str = "dev"
-    database_url: str = ""
-    rabbitmq_url: str = ""
-    queue_name: str = ""
+    database_url: AnyUrl
+    ampq_dsn: AmqpDsn
+    queue_name: str = Field(min_length=1)
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 class ConsumerSettings(ApiSettings):
-    weather_api_endpoint: str = ""
-    weather_api_token: str = ""
+    weather_api_endpoint: AnyUrl
+    weather_api_token: str = Field(min_length=1)
+    redis_dsn: RedisDsn
