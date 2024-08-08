0953fbd0409b5c63f15f4b74d82daba50b8cbb28
João Barreto 255 <34463115+JoaoBarreto255@users.noreply.github.com>
2024-08-07 23:49:17 -0300

    improved: created settings dependence injection

--- internal/settings.py
+++ internal/settings.py
@@ -1,5 +1,9 @@
 """Aplication settings"""
 
+from functools import lru_cache
+from typing import Annotated
+
+from fastapi import Depends
 from pydantic import AmqpDsn, AnyUrl, Field, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -17,3 +21,15 @@ class ConsumerSettings(ApiSettings):
     weather_api_endpoint: AnyUrl
     weather_api_token: str = Field(min_length=1)
     redis_dsn: RedisDsn
+
+@lru_cache
+def _api_settings_builder() -> ApiSettings:
+    return ApiSettings()
+
+ApiSettingsDI = Annotated[ApiSettings, Depends(_api_settings_builder)]
+
+@lru_cache
+def _consumer_settings_builder() -> ConsumerSettings:
+    return ConsumerSettings()
+
+ConsumerSettingsDI = Annotated[ConsumerSettings, Depends(_consumer_settings_builder)]
