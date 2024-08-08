8264133e54f6749cb55fae23136c51fa88bfd4f3
João Barreto 255 <34463115+JoaoBarreto255@users.noreply.github.com>
2024-08-08 19:18:47 -0300

    feat: convert to str using attributes

--- internal/settings.py
+++ internal/settings.py
@@ -11,8 +11,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class ApiSettings(BaseSettings):
     app_env: str = "dev"
     database_url: AnyUrl
-    amqp_dsn: AmqpDsn
+    amqp_url: AmqpDsn = Field(alias="AMQP_DSN")
     queue_name: str = Field(min_length=1)
+
+    @property
+    def database_dsn(self) -> str:
+        return str(self.database_url)
+
+    @property
+    def amqp_dsn(self) -> str:
+        return str(self.amqp_url)
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
@@ -22,14 +30,24 @@ class ConsumerSettings(ApiSettings):
     weather_api_token: str = Field(min_length=1)
     redis_dsn: RedisDsn
 
+    @property
+    def weather_api_dsn(self) -> str:
+        return str(self.weather_api_endpoint)
+
+
 @lru_cache
 def _api_settings_builder() -> ApiSettings:
     return ApiSettings()
 
+
 ApiSettingsDI = Annotated[ApiSettings, Depends(_api_settings_builder)]
+
 
 @lru_cache
 def _consumer_settings_builder() -> ConsumerSettings:
     return ConsumerSettings()
 
-ConsumerSettingsDI = Annotated[ConsumerSettings, Depends(_consumer_settings_builder)]
+
+ConsumerSettingsDI = Annotated[
+    ConsumerSettings, Depends(_consumer_settings_builder)
+]
