54e40e9a2a788a406bda4f7679cf193ff54328ba
João Barreto 255 <34463115+JoaoBarreto255@users.noreply.github.com>
2024-08-11 12:37:19 -0300

    feat: create repositories from prev tests

--- internal/database/repositories.py
+++ internal/database/repositories.py
@@ -6,7 +6,7 @@ from typing import Annotated
 from fastapi import Depends
 
 from internal.database.manager import AsyncDbManagerDI, AsyncDbManager
-from internal.models import User
+from internal.models import User, UserCityData, CityInfo, Base
 
 
 class BaseRepository:
@@ -14,6 +14,16 @@ class BaseRepository:
 
     def __init__(self, manager: AsyncDbManagerDI) -> None:
         self.database_manager = manager
+
+    async def insert(self, registry: Base) -> None:
+        """Save registry in database"""
+
+        await self.database_manager.insert_registry(registry)
+
+    async def update(self, registry: Base) -> None:
+        """Update user city data request in database"""
+
+        await self.database_manager.update_registry(registry)
 
 
 class UserRepository(BaseRepository):
@@ -24,7 +34,7 @@ class UserRepository(BaseRepository):
     async def new_user(self) -> User:
         """Create new user for download weather state"""
         user = User(created_at=datetime.now().isoformat())
-        await self.database_manager.insert_registry(user)
+        await self.insert(user)
 
         return user
 
@@ -35,11 +45,43 @@ class UserRepository(BaseRepository):
     async def remove_all_users(self) -> None:
         """Clear all users saved in database"""
         await self.database_manager.clear_model_registries(User)
-    
-    async def update_user(self, user: User) -> None:
-        """Update user"""
-
-        await self.database_manager.update_registry(User, user)
 
 
+class UserCityDataRepository(BaseRepository):
+    """Repository for user request data"""
+
+    async def get_all_user_city_data(self, user: User) -> list[UserCityData]:
+        """Get all user city data point from one user"""
+
+        return await self.database_manager.find_all_by_field(
+            UserCityData, "user_id", user.index
+        )
+
+
+class CityInfoRepository(BaseRepository):
+    """Repository to store Cities ids to request"""
+
+    async def new_cities(self, /, *identifiers) -> list[CityInfo]:
+        """Create one or more cities ids and store in db"""
+
+        result = []
+        for idx in identifiers:
+            city = CityInfo(api_id=idx)
+            await self.insert(city)
+            result.append(city)
+
+        return result
+
+    async def total_of_cities(self) -> int:
+        """Total of cities in database"""
+
+        return await self.database_manager.model_total_registries(CityInfo)
+
+
+CityInfoRepositoryDI = Annotated[
+    CityInfoRepository, Depends(CityInfoRepository)
+]
+UserCityDataRepositoryDI = Annotated[
+    UserCityDataRepository, Depends(UserCityDataRepository)
+]
 UserRepositoryDI = Annotated[UserRepository, Depends(UserRepository)]
